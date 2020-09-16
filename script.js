7cf3a95c3441b500ecc15684d4df62a0f4eecb38
Eduardo Pineda <edibozu@gmail.com>
2020-09-15 22:32:55 -0400

    created global variables in js file and also changed the name  of a few classes and id's in html file to make them easier to identify

---
+++ script.js
@@ -0,0 +1,22 @@
+// Assign global variables that will be used throughout the file.
+
+var apiKey = "3ef0d51604e6d21b745a6fbf9c9d0dd2";
+var searchBtn = $("#search-btn");
+var searchCity = $("#search-city");
+var clearBtn = $("#search-btn");
+var savedSearch = $("#saved-search");
+var forecastDisplay = $(".forecastDisplay");
+var cityDisplay = $("#city-display");
+var currentDate = moment().format('MMM Do');
+
+
+
+
+
+
+
+var queryURl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey
+$.ajax({
+    url: queryURL,
+    method: "GET",
+  })
