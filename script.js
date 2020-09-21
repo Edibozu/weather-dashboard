a9da9b99ad618501e53df0b393c1d97feb4dd0ae
Eduardo Pineda <edibozu@gmail.com>
2020-09-21 16:01:52 -0400

    made api call to show city name and also return weather stats, made styling changes to better display text, and added a class in index.html to target a change and dynamically displayed the stats

--- script.js
+++ script.js
@@ -10,18 +10,29 @@ var cityDisplay = $("#city-display");
 var currentDate = moment().format('MMM Do');
 var temp = $(".temp");
 var humidity = $(".humidity");
-var windSpeed = $(".widnSpeed");
+var windSpeed = $(".windSpeed");
 var uvIndex = $(".uvIndex");
 
+// on click event when you search for a city that will return temp stats.
 searchBtn.on("click", function(event){
     event.preventDefault();
-    var city = searchCity.val();
+    var city = searchCity.val().trim();
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
       console.log(response);
+      var cityLat = response.coord.lat;
+      var cityLon = response.coord.lon;
+      var cityCoords = "lat=" + cityLat + "&lon=" + cityLon + "&";
+      var foreCastURL = "https://api.openweathermap.org/data/2.5/onecall?" + cityCoords + "exclude=hourly,minutely&units=imperial&appid=" + apiKey;
+      cityDisplay.html("<h1>Today in " + response.name + ":</h1>");
+      temp.text("Current Temperature: " + response.main.temp + " °F");
+      humidity.text("Humidity: " + response.main.humidity + "%");
+      windSpeed.text("Wind Speed: " + response.wind.speed +  " mph");
+
+      
 
 
 
